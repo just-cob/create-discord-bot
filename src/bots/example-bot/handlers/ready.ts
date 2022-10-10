db710b4509e60e07d2923c853c4c7f23018b44b6
Cob <72044999+just-cob@users.noreply.github.com>
2022-10-09 20:56:13 -0700

    bot test
    plz work

--- src/bots/example-bot/handlers/ready.ts
+++ src/bots/example-bot/handlers/ready.ts
@@ -1,6 +1,21 @@
-import { Client } from "discord.js";
+import { BaseGuildTextChannel, Client } from "discord.js";
+import configs from "../config.json";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 module.exports = async (client: Client): Promise<void> => {
   console.log(__dirname.split("\\").slice(-2)[0]);
+
+  for (const config of configs) {
+    const channel = await client.channels.fetch(config.channelId);
+    if (!channel || channel.type === "DM") {
+      continue;
+    }
+
+    const message = await (channel as BaseGuildTextChannel).messages.fetch(
+      config.messageId
+    );
+
+    Object.keys(config.emojiRoleMap).forEach(
+      async (emoji) => await message.react(emoji)
+    );
+  }
 };
